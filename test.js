876d678380116a21c7096f8cf63c0cfbcaae745b
A K M Mahdi <94472254+AKM-Mahdi@users.noreply.github.com>
2022-09-26 22:36:48 +0600

    s

--- test.js
+++ test.js
@@ -1,9 +1,41 @@
-function squareSum(numbers) {
-  var result = 0;
-  for (var i in numbers) {
-    result = result + numbers[i] * numbers[i];
-  }
-  return result;
-}
+// function squareSum(numbers) {
+//   var result = 0;
+//   for (var i in numbers) {
+//     result = result + numbers[i] * numbers[i];
+//   }
+//   return result;
+// }
 
-console.log(squareSum([1, 2, 2]));
+// console.log(squareSum([1, 2, 2]));
+
+// console.log("before");
+// setTimeout(() => {
+//   console.log("inside...");
+// }, 2000);
+
+// console.log("after");
+
+console.log("before");
+getUser(1, function (user) {
+  console.log(user);
+  getRepo();
+  console.log("get repo fetching");
+});
+
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log("getting data from api");
+    const data = {
+      id: id,
+      getUserName: "Mahdi",
+    };
+    callback(data);
+  }, 2000);
+}
+console.log("after");
+
+function getRepo() {
+  setTimeout(() => {
+    console.log(["repo 1", "repo 2"]);
+  }, 5000);
+}
